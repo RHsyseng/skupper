1c12f8625d35d31e811f9a45445ceef95b5dc535
cwilkers <cwilkers@redhat.com>
2019-10-04 15:31:03 -0500

    Script to pull resources and export them for kustomization

---
+++ save-resources.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+OVERLAYS=mongo/overlays
+BASE=mongo/base
+NAMESPACE=skuppman-db
+
+for c in east1 east2 west2
+do
+    for res in $(oc --context $c -n $NAMESPACE \
+        get deployments,sa,secrets \
+        -o name | grep -v -E \
+        '(builder|default|deployer|dockercfg|token)' )
+    do
+        file="${OVERLAYS}/${c}/$(dirname $res)-$(basename $res)"
+        oc --context $c -n $NAMESPACE \
+            get $res -o yaml --export > $file
+    done
+done
+for res in $(oc --context east1 -n $NAMESPACE \
+    get roles,rolebindings \
+    -o name | grep -v -E \
+    '(system|token)' )
+do
+    file="${BASE}/$(dirname $res)-$(basename $res)"
+    oc --context east1 -n $NAMESPACE \
+        get $res -o yaml > $file
+done
+
+# Remove lines which create false differences
+sed -i -e '/^\s*creationTimestamp:/d' \
+       -e '/^\s*uid:/d' \
+       -e '/^\s*resourceVersion:/d' \
+       -e '/^\s*selfLink:/d' \
+       -e '/^\s*namespace:/d' \
+       mongo/overlays/*/* 
